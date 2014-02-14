4fa6eb5afd6258b905f410e82fb29c6a920ef7ea
mikusondhi2007 <mikusondhi2007@gmail.com>
2014-02-14 13:03:33 +0530

    git info added to gui

--- public/javascripts/rapport-clone.js
+++ public/javascripts/rapport-clone.js
@@ -37,19 +37,51 @@ RapportClone.prototype = {
         if (jqXHR.status == 200) {
           var tweets = {}
           var tw_user = data.tw[0].user;
-          var user_info = { name: tw_user.name, screen_name: tw_user.screen_name, description: tw_user.description, fav_count: tw_user.favourites_count, flwrs_count: tw_user.followers_count, following: tw_user.friends_count, pro_img: tw_user.profile_image_url }
-          $.each(data.tw, function(i, v){
+          var user_info = {
+            name: tw_user.name,
+            screen_name: tw_user.screen_name,
+            description: tw_user.description,
+            fav_count: tw_user.favourites_count,
+            flwrs_count: tw_user.followers_count,
+            following: tw_user.friends_count,
+            pro_img: tw_user.profile_image_url
+          }
+          $.each(data.tw, function(i, v) {
             tweets[i] = v.text
           });
-
+          var repos = {};
+          $.each(data.gh, function(i, v) {
+            repos[i] = {
+              clone_url: v.clone_url,
+              collaborators_url: v.collaborators_url,
+              created_at: v.created_at,
+              default_branch: v.default_branch,
+              description: v.description,
+              forks_count: v.forks_count,
+              full_name: v.full_name,
+              git_url: v.git_url,
+              html_url: v.html_url,
+              has_wiki: v.has_wiki,
+              language: v.language,
+              master_branch: v.master_branch,
+              private: v.private,
+              ssh_url: v.ssh_url,
+              url: v.url,
+              watchers: v.watchers
+            }
+          });
+          var gh_user_info = data.gh[0].owner;
           $('.query-block').hide();
           $('.response-block').show();
-          $('.fb-res').append("<span>"+JSON.stringify(data.fb)+"</span>&nbsp<img src=https://graph.facebook.com/"+data.fb.id+"/picture />");
-          // debugger; 
-          $('.yw-res').append("<span>"+JSON.stringify(data.yt)+"</span>&nbsp<img src="+user_info.pro_img+" />");
-          $('.tw-res .user-info').append("<user>"+ JSON.stringify(user_info) +"</user>");
-          $('.tw-res .tweets').append("<tweets>"+ JSON.stringify(tweets) +"</tweets>");
-          $('.gm-res').append("<img src="+data.gm+" />");
+          $('.fb-res').append("<span>" + JSON.stringify(data.fb) + "</span>&nbsp<img src=https://graph.facebook.com/" + data.fb.id + "/picture />");
+          // debugger;
+          $('.yw-res').append("<span>" + JSON.stringify(data.yt) + "</span>&nbsp<img src=" + user_info.pro_img + " />");
+          $('.tw-res .user-info').append("<user>" + JSON.stringify(user_info) + "</user>");
+          $('.tw-res .tweets').append("<tweets>" + JSON.stringify(tweets) + "</tweets>");
+          $('.gm-res').append("<img src=" + data.gm + " />");
+          $('.gh-res .gh-user-info').append("<user>" + JSON.stringify(gh_user_info) + "</user> &nbsp<img src=" + gh_user_info.avatar_url + " />");
+          // debugger;
+          $('.gh-res .gh-repos').append("<repos>" + JSON.stringify(repos) + "</repos>");
         }
       }
     });
